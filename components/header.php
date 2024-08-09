3a05709ff713feb3eb8127218387ce3e4ddfa81f
Parsa Mostafaie <155385612+parsa-mostafaie@users.noreply.github.com>
2024-08-09 18:59:05 +0330

    Add responsive meta tag

--- components/header.php
+++ components/header.php
@@ -8,6 +8,7 @@ include_once __DIR__ . '/../init.php';
 <head>
   <meta charset="UTF-8">
   <title><?= meta('title', "فروشگاه اینترنتی") ?></title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="<?= c_url('/css/style.css', false); ?>">
   <?php do_head() ?>
 </head>
