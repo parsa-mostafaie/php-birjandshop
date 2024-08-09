429d866e44b28e66a14d5b137c15f236c3a3c124
Parsa Mostafaie <155385612+parsa-mostafaie@users.noreply.github.com>
2024-08-09 17:02:29 +0330

    User Model Docblock

--- Core/Models/User.php
+++ Core/Models/User.php
@@ -3,13 +3,51 @@ namespace Birjandshop\Models;
 
 use pluslib\App\Models\User as ModelsUser;
 
+/**
+ * @property int $id
+ * 
+ * @property string $username
+ * @property string $email
+ * 
+ * @property string $password
+ * 
+ * @property string $name
+ * @property string $firstname
+ * @property string $family
+ * @property string $lastname
+ * 
+ * @property string $phone
+ * 
+ * @property string $country
+ * @property string $state
+ * @property string $city
+ * @property string $address
+ * 
+ * @property string $zip_code
+ * 
+ * @property int|float $total_order
+ * @property int $order_count
+ * 
+ * @property string|int|\pluslib\Database\Expression $register_date
+ */
 class User extends ModelsUser
 {
-    protected $id_field = "ID";
+  protected $id_field = "ID";
 
-    private function set_password($password)
-    {
-        $this->password = hash_pass($password);
-    }
+  protected $translation = [
+    'firstname' => 'name',
+    'lastname' => 'family'
+  ];
+
+  const updated_at = null;
+  const created_at = 'register_date';
+
+  function set_password($password = null, $save = true)
+  {
+    $password ??= $this->username;
+    $this->password = hash_pass($password);
+
+    $save && $this->save();
+  }
 
 }
