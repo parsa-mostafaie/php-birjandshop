59d0fe4e7962bdf48dd96f43a0c69a8a8d6e7b56
Parsa Mostafaie <155385612+parsa-mostafaie@users.noreply.github.com>
2024-08-09 17:50:47 +0330

    Customize 404.php

--- 404.php
+++ 404.php
@@ -1,16 +1,17 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>404 - Not Found</title>
-</head>
-
-<body>
-    <h1>HTTP/1.1 404 - Not Found</h1>
-    <code><?= $_SERVER['REQUEST_URI'] ?></code> Was Not found!
-    <hr />
-</body>
-
-</html>
+<?php
+include_once 'init.php';
+pls_http_response_code(404, live: true);
+?>
+<?php include 'components/header.php'; ?>
+<section class="container">
+  <main class="d-flex justify-content-center align-items-center">
+    <h1 class="ms-3 ps-3 align-top border-start inline-block align-content-center">404</h1>
+    <div class="inline-block align-middle">
+      <h2 class="font-weight-normal lead" id="desc">صفحه مورد نظر شما یافت نشد!</h2>
+    </div>
+  </main>
+  <footer class="d-flex w-100 justify-content-center">
+    <a href="<?= url(c_url('/')) ?>" class="btn btn-outline-primary">رفتن به صفحه اصلی</a>
+  </footer>
+</section>
+<?php include 'components/footer.php'; ?>
