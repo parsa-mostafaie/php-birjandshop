192669fcbb0cd449533f10ff42c5fd5ac973fa66
Parsa Mostafaie <155385612+parsa-mostafaie@users.noreply.github.com>
2024-08-10 20:06:14 +0330

    View fallback

--- Core/Functions/functions-view.php
+++ Core/Functions/functions-view.php
@@ -13,6 +13,9 @@ function view($url, $id = null, $props = [], $without_loading = false)
         <div class="spinner-grow text-primary" role="status" aria-hidden="true"></div>
         <p class="my-0 me-2">لطفا صبر کنید!</p>
       </div>
+      <div class="fallback text-danger d-none d-flex justify-content-center align-items-center mx-auto">
+        خطا هنگام بارگزاری...
+      </div>
     <?php endif; ?>
     <div ajax-content http-method="post" href="<?= redirect($www, back: true, gen: true) ?>" loading=".loading">
     </div>
